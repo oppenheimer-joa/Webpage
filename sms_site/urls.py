a0a4ed548e27817258f386912806aa79a9d7107e
nayeon1107 <quf991107@gmail.com>
2023-09-13 17:59:30 +0900

    230913/na S3 parquet 여러개 불러오기 + 검색 기능 추가

--- sms_site/urls.py
+++ sms_site/urls.py
@@ -4,5 +4,8 @@ from sms_site.views import *
 
 urlpatterns = [
     # path('', dictionary),
-    path('', home)
+    path('', home),
+    path('genre/', genre_list),
+    path('movie/', dictionary, name='dictionary'),
+    path('movie/<str:genre>/', movie_filter_by_genre, name='moviegenre')
 ]
--- sms_site/views.py
+++ sms_site/views.py
@@ -1,62 +1,109 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from configparser import ConfigParser
 from storages.backends.s3boto3 import S3Boto3Storage
 import json
 from django.db.models import Q
 import pandas as pd
-import io
+import boto3
+from io import BytesIO
+import pyarrow.parquet as pq
 
-# Create your views here.
-# def index(request):
-#     storage = S3Boto3Storage()
-#     json_file = storage.open('TMDB/detail/2023-01-06/TMDB_movieDetails_1001800_2023-01-06.json')
-#     data = json_file.read()
-#     # JSON 데이터 디코딩
-#     decoded_data = json.loads(data.decode('utf-8'))
-
-#     original_title = decoded_data["original_title"]
-#     poster_path = decoded_data["poster_path"]
-#     json_file.close()
-#     return render(request, 'index.html', {"movie_title": original_title, "movie_poster": poster_path})
-
+    
 def dictionary(request):
-    storage = S3Boto3Storage()
-    directory = 'TMDB/2023-07-14/TMDB_movie_872585_2023-07-14'  # 원하는 디렉토리 경로
 
-    # 디렉토리 내의 모든 파일 목록 가져오기
-    directories, files = storage.listdir(directory)
-    print(files)
+    parser = ConfigParser()
+    parser.read("./config/config.ini")
+    access = parser.get("AWS", "S3_ACCESS")
+    secret = parser.get("AWS", "S3_SECRET")
+
+    s3 = boto3.client('s3', aws_access_key_id=access, aws_secret_access_key=secret)
+    objects = s3.list_objects_v2(Bucket='sms-warehouse', Prefix='TMDB/2023-07-14/')
+
 
     movie_details = pd.DataFrame(columns=[
         'id', 'cast', 'crew', 'backdrop_path', 'belongs_to_collection',
-       'budget', 'genres', 'homepage', 'imdb_id', 'original_language',
-       'original_title', 'overview', 'poster_path', 'production_companies',
-       'production_countries', 'release_date', 'revenue', 'runtime',
-       'spoken_languages', 'video', 'results', 'backdrops', 'logos',
-       'posters'
-    ])  # 일단 인물 정보로 parquet test
+        'budget', 'genres', 'homepage', 'imdb_id', 'original_language',
+        'original_title', 'overview', 'poster_path', 'production_companies',
+        'production_countries', 'release_date', 'revenue', 'runtime',
+        'spoken_languages', 'video', 'results', 'backdrops', 'logos',
+        'posters'
+    ])
 
-    for idx, file_name in enumerate(files):
-        if file_name == '_SUCCESS' :
+    for obj in objects.get('Contents'):
+        file_path = 's3://{}/{}'.format('sms-warehouse', obj.get('Key'))
+        print(file_path)
+        if file_path.find('parquet') == -1:
             continue
-        file = storage.open(f"{directory}/{file_name}")
-        pq_file = pd.read_parquet(file)
-        movie_details = pd.concat([movie_details, pq_file], ignore_index=True)
-        # data = json_file.read()
-        # json_file.close()
-        # decoded_data = json.loads(data.decode('utf-8'))
-        # original_title = decoded_data["original_title"]
-        # poster_path = decoded_data["poster_path"]
-        # movie_details.append({"movie_title": pq_file})
-        if idx >= 30 :
-            break
-    
+        s3_object = s3.get_object(Bucket='sms-warehouse', Key=obj.get('Key'))
+        parquet_data = BytesIO(s3_object['Body'].read())
+        parquet_table = pq.read_table(parquet_data)
+        parquet_df = parquet_table.to_pandas()
+        movie_details = pd.concat([movie_details, parquet_df], ignore_index=True)
+
+    # 장르 endpoint 가 붙으면
+    s3 = boto3.client('s3', aws_access_key_id=access, aws_secret_access_key=secret)
+    objects = s3.list_objects_v2(Bucket='sms-warehouse', Prefix='genre/2023-07-14/')
+
+    genre_df = pd.DataFrame()
+
+    for obj in objects.get('Contents'):
+        file_path = 's3://{}/{}'.format('sms-warehouse', obj.get('Key'))
+        print(file_path)
+        if file_path.find('parquet') == -1:
+            continue
+        s3_object = s3.get_object(Bucket='sms-warehouse', Key=obj.get('Key'))
+        parquet_data = BytesIO(s3_object['Body'].read())
+        parquet_table = pq.read_table(parquet_data)
+        parquet_df = parquet_table.to_pandas()
+        genre_df = pd.concat([genre_df, parquet_df], ignore_index=True)
+        
+    movie_by_genre_list = genre_df[genre_df['Drama'] == 1]['id'].tolist()
+    # 장르 endpoint 코드 끝
+
+    search_type = request.GET.get('type', '')
     search = request.GET.get('search', '')
-    if search: # search 값이 있으면
-        movie_details = [movie for movie in movie_details if search in movie["movie_title"]]
-    return render(request, 'sms_site/dictionary.html',{"movie_list": movie_details})
+    if search != "": # search 값이 있으면
+        if search_type == '제목' :
+            movie_details = movie_details[movie_details['original_title'].str.contains(search)]
+        if search_type == '내용' :
+            movie_details = movie_details[movie_details['overview'].str.contains(search)]
+        if search_type == '감독' :
+            movie_details = movie_details
+    return render(request, 'sms_site/dictionary.html',{"movie_list": movie_details,
+                                                       "search":search,
+                                                       "search_type":search_type})
 
 
+def genre_list(request):
+    return render(request, "sms_site/dictionary_genre.html")
+
+def movie_filter_by_genre(request,genre):
+
+    parser = ConfigParser()
+    parser.read("./config/config.ini")
+    access = parser.get("AWS", "S3_ACCESS")
+    secret = parser.get("AWS", "S3_SECRET")
+    
+    s3 = boto3.client('s3', aws_access_key_id=access, aws_secret_access_key=secret)
+    objects = s3.list_objects_v2(Bucket='sms-warehouse', Prefix='genre/2023-07-14/')
+
+    genre_df = pd.DataFrame()
+
+    for obj in objects.get('Contents'):
+        file_path = 's3://{}/{}'.format('sms-warehouse', obj.get('Key'))
+        print(file_path)
+        if file_path.find('parquet') == -1:
+            continue
+        s3_object = s3.get_object(Bucket='sms-warehouse', Key=obj.get('Key'))
+        parquet_data = BytesIO(s3_object['Body'].read())
+        parquet_table = pq.read_table(parquet_data, filters=[(id,'=',1)])
+        parquet_df = parquet_table.to_pandas()
+        genre_df = pd.concat([genre_df, parquet_df], ignore_index=True)
+        
+    movie_by_genre_list = genre_df[genre_df['Drama'] == 1]['id'].tolist()
+
+
+    return render(request, "sms_site/dictionary.html")
 
 def home(request):
     return render(request, "sms_site/home.html")
