3bc9512de5e83ced115f25c2cc24d5d501ad5309
hooniegit <neivekim76@gmail.com>
2023-09-19 15:51:41 +0900

    230919/hooneigit 손가락 아이콘 추가

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
